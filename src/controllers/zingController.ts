0f6364f0a476d7f7c88d0dc3b9be5b54c4380c9b
hieuntcasso <94815095+hieuntcasso@users.noreply.github.com>
2022-04-06 22:05:42 +0700

    add full api

---
+++ src/controllers/zingController.ts
@@ -0,0 +1,230 @@
+import { Request, Response, NextFunction } from 'express';
+import config from '../config'
+import fs from 'fs';
+import UserModel from '../models/user.model';
+import { Base64 } from 'js-base64';
+
+import { v4 as uuidv4 } from 'uuid';
+import { OAuth2Client } from 'google-auth-library';
+import { generateToken } from '../middlewares/auth.mdw';
+
+const client = new OAuth2Client(process.env.GOOGLE_AUTH_CLIENT_ID);
+const db = new UserModel()
+const ZingMp3 = require("zingmp3-api-full")
+
+export default class zingController {
+    // not params
+    getTop100(req: Request, res: any, next: NextFunction) {
+        ZingMp3.getTop100()
+            .then((data) => {
+                console.log(data)
+                return res.status(200).send({ data });
+            })
+            .catch((err) => {
+                console.log(err)
+                return res.status(500).send({ err });
+            })
+    }
+    async getChartHome(req: Request, res: any, next: NextFunction) {
+        ZingMp3.getChartHome()
+            .then((data) => {
+                console.log(data)
+                return res.status(200).send({ data });
+            })
+            .catch((err) => {
+                console.log(err)
+                return res.status(500).send({ err });
+            })
+    }
+    async getNewReleaseChart(req: Request, res: any, next: NextFunction) {
+        ZingMp3.getNewReleaseChart()
+            .then((data) => {
+                console.log(data)
+                return res.status(200).send({ data });
+            })
+            .catch((err) => {
+                console.log(err)
+                return res.status(500).send({ err });
+            })
+    }
+    // 1 params
+    async getSong(req: Request, res: any, next: NextFunction) {
+        if (!req.query.id) {
+            return res.status(500).send({ mess: 'Lỗi định dạng, xem lại id' });
+        }
+        let id = req.query.id;
+
+        ZingMp3.getSong(id)
+            .then((data) => {
+                console.log(data)
+                return res.status(200).send({ data });
+            })
+            .catch((err) => {
+                console.log(err)
+                return res.status(500).send({ err });
+            })
+    }
+    async getDetailPlaylist(req: Request, res: any, next: NextFunction) {
+        if (!req.query.id) {
+            return res.status(500).send({ mess: 'Lỗi định dạng, xem lại id' });
+        }
+        let id = req.query.id;
+
+        ZingMp3.getDetailPlaylist(id)
+            .then((data) => {
+                console.log(data)
+                return res.status(200).send({ data });
+            })
+            .catch((err) => {
+                console.log(err)
+                return res.status(500).send({ err });
+            })
+
+    }
+    async getHome(req: Request, res: any, next: NextFunction) {
+        if (!req.query.id) {
+            return res.status(500).send({ mess: 'Lỗi định dạng, xem lại id' });
+        }
+        let id = req.query.id;
+
+        ZingMp3.getHome(id)
+            .then((data) => {
+                console.log(data)
+                return res.status(200).send({ data });
+            })
+            .catch((err) => {
+                console.log(err)
+                return res.status(500).send({ err });
+            })
+    }
+    async getInfoSong(req: Request, res: any, next: NextFunction) {
+        if (!req.query.id) {
+            return res.status(500).send({ mess: 'Lỗi định dạng, xem lại id' });
+        }
+        let id = req.query.id;
+
+        ZingMp3.getInfoSong(id)
+            .then((data) => {
+                console.log(data)
+                return res.status(200).send({ data });
+            })
+            .catch((err) => {
+                console.log(err)
+                return res.status(500).send({ err });
+            })
+
+    }
+    async getArtist(req: Request, res: any, next: NextFunction) {
+        if (!req.query.name) {
+            return res.status(500).send({ mess: 'Lỗi định dạng, xem lại name' });
+        }
+        let name = req.query.name;
+
+        ZingMp3.getArtist(name)
+            .then((data) => {
+                console.log(data)
+                return res.status(200).send({ data });
+            })
+            .catch((err) => {
+                console.log(err)
+                return res.status(500).send({ err });
+            })
+
+    }
+    async getLyric(req: Request, res: any, next: NextFunction) {
+        if (!req.query.id) {
+            return res.status(500).send({ mess: 'Lỗi định dạng, xem lại id' });
+        }
+        let id = req.query.id;
+
+        ZingMp3.getLyric(id)
+            .then((data) => {
+                console.log(data)
+                return res.status(200).send({ data });
+            })
+            .catch((err) => {
+                console.log(err)
+                return res.status(500).send({ err });
+            })
+    }
+    async search(req: Request, res: any, next: NextFunction) {
+        if (!req.query.name) {
+            return res.status(500).send({ mess: 'Lỗi định dạng, xem lại name' });
+        }
+        let name = req.query.name;
+
+        ZingMp3.search(name)
+            .then((data) => {
+                console.log(data)
+                return res.status(200).send({ data });
+            })
+            .catch((err) => {
+                console.log(err)
+                return res.status(500).send({ err });
+            })
+    }
+    async getCategoryMV(req: Request, res: any, next: NextFunction) {
+        if (!req.query.id) {
+            return res.status(500).send({ mess: 'Lỗi định dạng, xem lại id' });
+        }
+        let id = req.query.id;
+
+        ZingMp3.getCategoryMV(id)
+            .then((data) => {
+                console.log(data)
+                return res.status(200).send({ data });
+            })
+            .catch((err) => {
+                console.log(err)
+                return res.status(500).send({ err });
+            })
+
+    }
+    async getVideo(req: Request, res: any, next: NextFunction) {
+        if (!req.query.id) {
+            return res.status(500).send({ mess: 'Lỗi định dạng, xem lại id' });
+        }
+        let id = req.query.id;
+
+        ZingMp3.getVideo(id)
+            .then((data) => {
+                console.log(data)
+                return res.status(200).send({ data });
+            })
+            .catch((err) => {
+                console.log(err)
+                return res.status(500).send({ err });
+            })
+    }
+    async getListMV(req: Request, res: any, next: NextFunction) {
+        if (!req.query.id || !req.query.page || !req.query.count) {
+            return res.status(500).send({ mess: 'Lỗi định dạng, xem lại name' });
+        }
+        let id = req.query.id;
+        let page = req.query.page;
+        let count = req.query.count;
+
+        ZingMp3.getListMV(id, page, count)
+            .then((data) => {
+                console.log(data)
+                return res.status(200).send({ data });
+            })
+            .catch((err) => {
+                console.log(err)
+                return res.status(500).send({ err });
+            })
+    }
+}
+
+function handleError(res, data) {
+    if (data == 0)
+        return res.status(200).send({ code: '00', desc: 'success' });
+    return res.status(400).send({ code: '01', desc: 'fail' });
+}
+
+
+
+
+
+
+
--- src/routes/Route.ts
+++ src/routes/Route.ts
@@ -1,24 +1,45 @@
 import { Router } from 'express';
-import TugoController from '../controllers/tugoController';
+import ZingController from '../controllers/zingController';
 import { verifyToken } from '../middlewares/auth.mdw';
 import apicache from 'apicache';
 let cache = apicache.middleware;
 class Routes {
     router = Router();
-    tugoController = new TugoController()
+    zingController = new ZingController()
     constructor() {
         this.intializeRoutes();
     }
     intializeRoutes() {
-        this.router.route('/')
-            .get(this.tugoController.home);
-        this.router.route('/insert')
-            .post(this.tugoController.insertRow);
-        this.router.route('/delete')
-            .delete(this.tugoController.deleteRow);
-        this.router.route('/get/tour')
-            .get(this.tugoController.getTour);
-       
+        this.router.route('/top-100')
+            .get(this.zingController.getTop100);
+        this.router.route('/chart-home')
+            .get(this.zingController.getChartHome);
+        this.router.route('/new-release-chart')
+            .get(this.zingController.getNewReleaseChart);
+        // 1 param (:id)        
+        this.router.route('/song')
+            .get(this.zingController.getSong);
+        this.router.route('/detail-playlist')
+            .get(this.zingController.getDetailPlaylist);
+        this.router.route('/home')
+            .get(this.zingController.getHome);
+        this.router.route('/song-info')
+            .get(this.zingController.getInfoSong);
+        this.router.route('/artist')
+            .get(this.zingController.getArtist);
+        this.router.route('/lyric')
+            .get(this.zingController.getLyric);
+        this.router.route('/search')
+            .get(this.zingController.search);
+        this.router.route('/artist')
+            .get(this.zingController.getArtist);
+        this.router.route('/category-mv')
+            .get(this.zingController.getCategoryMV);
+        this.router.route('/video-mv')
+            .get(this.zingController.getVideo);
+        // 3 params
+        this.router.route('/list-mv')
+            .get(this.zingController.getListMV);
     }
 }
 
